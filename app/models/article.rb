48c1b6501c5f69fd209ee14e0fa8f7faeea217c3
Emily O'Marra <emilyomarra@hotmail.com>
2023-04-29 04:29:24 +0900

    add ransack gem, create search controller, and add search bar on search page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,14 @@
 class Article < ApplicationRecord
   include Visible
 
+  def self.ransackable_attributes(auth_object = nil)
+    ["title"]
+  end
+
   has_many :comments, dependent: :destroy
   has_one_attached :photo
 
   validates :body, presence: true, length: { minimum: 10 }
   validates :title, :category, :prefecture, presence: true
+
 end
