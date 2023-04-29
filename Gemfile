786c1b98b5acab98576a34f48f449c31533d530d
Emily O'Marra <emilyomarra@hotmail.com>
2023-04-29 12:11:49 +0900

    before devise add

--- Gemfile
+++ Gemfile
@@ -60,6 +60,8 @@ gem "font-awesome-sass", "~> 6.1"
 
 gem 'jquery-rails'
 
+gem 'pg_search'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,13 @@
 class Article < ApplicationRecord
   include Visible
 
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_body,
+  against: [ :title, :body ],
+  using: {
+    tsearch: { prefix: true } #
+}
+
   def self.ransackable_attributes(auth_object = nil)
     ["title"]
   end
